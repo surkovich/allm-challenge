7a8e2646644edd0936f4105c413fd2736f5278c8
Aleksandr Babak <surkovich@gmail.com>
2023-06-21 21:20:04 +0900

    cleanup

--- src/main/kotlin/net/allm/challenge/repository/impl/entity/HospitalEntity.kt
+++ src/main/kotlin/net/allm/challenge/repository/impl/entity/HospitalEntity.kt
@@ -9,6 +9,6 @@ internal class HospitalEntity {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id", nullable = false)
-        open var id: Long? = null
+        var id: Long? = null
         lateinit var name: String
 }
